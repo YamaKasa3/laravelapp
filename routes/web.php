fec5dc74930046b60e643eb7b6e0a78fed624208
YamaKasa3 <yt.const@gmail.com>
2020-12-28 23:47:00 +0900

    parameter

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('hello', function() {
 });
 */
 
+
 // ヒアドキュメント
 $html = <<<EOF
 <html>
@@ -41,4 +42,27 @@ EOF;
 
 Route::get('hello', function () use ($html) {
     return $html;
+});
+
+
+// パラメータの利用
+Route::get('hello/{msg}', function ($msg) {
+    $html = <<< EOF
+    <html>
+    <head>
+    <title>Hello</title>
+    <style>
+    body { font-size:16px; color:#999; }
+    h1 { font-size:100px; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+    </style>
+    <body>
+        <h1>Hello</h1>
+        <p>{$msg}</p>
+        <p>これは、サンプルで作ったページです。</p>
+    </body>
+    </html>
+EOF;
+
+    return $html;
+
 });
