c6f23fc6e53d45e233071632f47b1af0f65314c0
YamaKasa3 <yt.const@gmail.com>
2020-12-28 23:36:41 +0900

    ヒアドキュメント

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*
 Route::get('hello', function() {
     return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+});
+*/
+
+// ヒアドキュメント
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body { font-size:16px; color:#999; }
+h1 { font-size:100px; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+</style>
+<body>
+    <h1>Hello</h1>
+    <p>This is sample page.</p>
+    <p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
+
+Route::get('hello', function () use ($html) {
+    return $html;
 });
